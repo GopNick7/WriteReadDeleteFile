29c4a67841a807882d20c9357f49e9578ffb3e57
Nikita <nekit88j@gmail.com>
2017-05-25 17:19:13 +0300

    Create method saveData

--- app/src/main/java/com/justappp/nekitpc/writereaddeletefile/MainActivity.java
+++ app/src/main/java/com/justappp/nekitpc/writereaddeletefile/MainActivity.java
@@ -2,12 +2,53 @@ package com.justappp.nekitpc.writereaddeletefile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtText;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtText = (EditText) findViewById(R.id.edtText);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData("TestFileName",edtText.getText().toString());
+                Toast.makeText(MainActivity.this, R.string.fileSaved, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void saveData(String fileName, String data) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, MODE_PRIVATE)));
+            writer.write(data);
+
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
