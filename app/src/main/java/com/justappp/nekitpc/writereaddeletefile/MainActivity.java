5b3bb2bbc4008da5745aa3617487f33262ce8abd
Nikita <nekit88j@gmail.com>
2017-05-25 16:39:06 +0300

    Create project with blank activity and layout

---
+++ app/src/main/java/com/justappp/nekitpc/writereaddeletefile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.justappp.nekitpc.writereaddeletefile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
