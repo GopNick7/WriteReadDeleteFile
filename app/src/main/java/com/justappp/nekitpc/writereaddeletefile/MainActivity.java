4196056748788c2aa0638171ea4bac10f53f2236
Nikita <nekit88j@gmail.com>
2017-05-25 19:06:38 +0300

    Rename method saveData to writeData, add method readData

--- app/src/main/java/com/justappp/nekitpc/writereaddeletefile/MainActivity.java
+++ app/src/main/java/com/justappp/nekitpc/writereaddeletefile/MainActivity.java
@@ -5,41 +5,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.CharBuffer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText edtText;
-    Button btnSave;
+    private static final String FILE_NAME = "TestFileName";
+
+    private EditText edtWrite;
+    private TextView txtRead;
+    private Button btnWrite, btnRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtText = (EditText) findViewById(R.id.edtText);
+        edtWrite = (EditText) findViewById(R.id.edtWrite);
+        txtRead = (TextView) findViewById(R.id.txtRead);
 
-        btnSave = (Button) findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveData("TestFileName",edtText.getText().toString());
-                Toast.makeText(MainActivity.this, R.string.fileSaved, Toast.LENGTH_SHORT).show();
-            }
-        });
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+        btnRead = (Button) findViewById(R.id.btnRead);
+
+        btnWrite.setOnClickListener(this);
+        btnRead.setOnClickListener(this);
     }
 
-    private void saveData(String fileName, String data) {
+    private void writeData(String fileName, String data) {
         BufferedWriter writer = null;
         try {
             writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, MODE_PRIVATE)));
             writer.write(data);
-
         } catch (java.io.IOException e) {
             e.printStackTrace();
         } finally {
@@ -49,6 +53,42 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
 
+    private String readData(String fileName){
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
+
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                return line;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnWrite:
+                writeData(FILE_NAME, edtWrite.getText().toString());
+                Toast.makeText(MainActivity.this, R.string.fileWritten, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnRead:
+                txtRead.setText(readData(FILE_NAME));
+                Toast.makeText(MainActivity.this, R.string.fileReadOut, Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
